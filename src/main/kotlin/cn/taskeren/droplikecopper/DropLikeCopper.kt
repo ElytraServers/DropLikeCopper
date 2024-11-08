93f5c31915544cac9dbd78991a6b6c62484a9a01
Taskeren <r0yalist@outlook.com>
2024-11-09 05:10:25 +0800

    fixed the crashing bug

--- src/main/kotlin/cn/taskeren/droplikecopper/DropLikeCopper.kt
+++ src/main/kotlin/cn/taskeren/droplikecopper/DropLikeCopper.kt
@@ -1,14 +1,12 @@
 package cn.taskeren.droplikecopper
 
 import net.neoforged.api.distmarker.Dist
-import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.fml.common.Mod
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
 @Mod(DropLikeCopper.ID, dist = [Dist.DEDICATED_SERVER])
-@EventBusSubscriber
 object DropLikeCopper {
 	const val ID = "drop_like_copper"
 
